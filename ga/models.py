e1368ecde2e2198e0b6bfc31e4752226904dad1f
Ellie <web@stillinbeta.com>
2011-02-22 02:53:08 -0500

    Pulls car data into database and demonstrably retrieves it

---
+++ ga/models.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import db
+
+class StreetcarLocation(db.Model):
+#    num = db.IntegerProperty() #we're using key_name as car number
+    location = db.GeoPtProperty()
+    route = db.StringProperty()
+
--- ga/update.py
+++ ga/update.py
@@ -1,19 +1,28 @@
 from google.appengine.ext import webapp
 from google.appengine.api.urlfetch import fetch
+from google.appengine.ext import db
 from xml.dom import minidom
-routes = ['510']
+from models import StreetcarLocation
+routes = range(501,513)
+api_url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r={0}&t=0'
 
 
 class LocationUpdater(webapp.RequestHandler):
     def get(self):
-        response = fetch('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r={0}&t=0'.format(routes[0]))
-        tree = minidom.parseString(response.content)
-        vehicles = {}
-        for vehicle in tree.getElementsByTagName('vehicle'):
-            if vehicle.getAttribute('predictable') == u'true':
-                vehicles[vehicle.getAttribute('id')] = ( vehicle.getAttribute('lat'), 
-                vehicle.getAttribute('lon'))
-
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(vehicles)
+        for route in routes:
+            response = fetch(api_url.format(route))
+            tree = minidom.parseString(response.content)
+            for vehicle in tree.getElementsByTagName('vehicle'):
+                if vehicle.getAttribute('predictable') == u'true':
+                    car_id = int(vehicle.getAttribute('id'))
+                    self.response.out.write(car_id)
+                    car = StreetcarLocation.get_or_insert(key_name=str(car_id))
+                    car.location = (vehicle.getAttribute('lat')+","
+                                    +vehicle.getAttribute('lon'))
+                    car.route = route
+                    car.put()
+        rpc = db.create_rpc(deadline=5, read_policy=db.EVENTUAL_CONSISTENCY) 
+        for car in StreetcarLocation.all().run(rpc=rpc):
+            self.response.out.write("{0} at {1} on route {2}\n".format(car.key().name(),car.location,car.route))
         
