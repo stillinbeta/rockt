5cab0ccc9a7c4504cdab6dfac4b575d166ca389d
stillinbeta <web@stillinbeta.com>
2011-02-28 01:56:52 -0500

    Added method to remove stale streetcars

--- ga/update.py
+++ ga/update.py
@@ -7,16 +7,19 @@ routes = range(501,513)
 api_url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r=%s&t=0'
 
 
+cars_updated = []
+
 def process_response(rpc,route):
     response = rpc.get_result()
     tree = minidom.parseString(response.content)
     for vehicle in tree.getElementsByTagName('vehicle'):
-        car_id = int(vehicle.getAttribute('id'))
-        car = StreetcarLocation.get_or_insert(key_name=str(car_id))
+        car_id = vehicle.getAttribute('id')
+        car = StreetcarLocation.get_or_insert(key_name=car_id)
         if vehicle.getAttribute('predictable') == u'true':
             car.location = (vehicle.getAttribute('lat')+","
                             +vehicle.getAttribute('lon'))
             car.route = route
+            cars_updated.append(car_id)
             car.in_service = True
         else:
             car.in_service = False
@@ -25,6 +28,21 @@ def process_response(rpc,route):
   
 def create_callback(rpc,route):
     return lambda: process_response(rpc,route)
+
+def remove_out_of_service():
+    rpc = db.create_rpc(deadline=10, read_policy=db.EVENTUAL_CONSISTENCY)
+    query = StreetcarLocation.all()
+    all_streetcars = query.filter("in_service = ",True).run(rpc=rpc)
+
+    changed = []
+    for streetcar in all_streetcars:
+        key = streetcar.key().name()
+        if key not in cars_updated:
+            streetcar.in_service = False
+            changed.append(streetcar)
+    if changed:
+        rpc = db.create_rpc(deadline=10, read_policy=db.EVENTUAL_CONSISTENCY)
+        db.put(changed)
 
 rpcs = []
 for route in routes:
@@ -36,7 +54,7 @@ for route in routes:
 
 print "Content-Type: text/plain"
 apiproxy_stub_map.UserRPC.wait_all(rpcs)
-print "All Routes Processed"
-
-
+print "All Routes Updated"
+remove_out_of_service()
+print "Updated is complete"
 
