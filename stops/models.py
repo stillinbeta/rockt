5a41c2199a87ae45759b4a1e7bd33dc79e9cb33f
stillinbeta <web@stillinbeta.com>
2011-09-27 13:27:22 -0400

    Finished updater scripts for stops and cars

--- stops/models.py
+++ stops/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Stop(models.Model):
+    number = models.TextField()
+    route = models.IntegerField()
+    description = models.TextField()
+    geohash = models.TextField()
+    latitude = models.DecimalField(max_digits=9,decimal_places=6,null=True)
+    longitude = models.DecimalField(max_digits=9,decimal_places=6,null=True)
+    
+    def save(self, *args, **kwargs):
+        try:
+            old = Stop.objects.get(number=self.number)
+            self.id  = old.id
+        except Stop.DoesNotExist:
+            pass
+        super(Stop,self).save(*args,**kwargs)
--- update.py
+++ update.py
@@ -1,15 +1,44 @@
+from os import getcwd
+from shutil import rmtree
+import argparse
+
 from django.core.management import setup_environ
 import settings
 setup_environ(settings)
 
 from rockt.cars.update import update_streetcars, remove_out_of_service
+from rockt.stops.update import update_stops, retrieve_data 
 
-routes = range(501,513)
-route_list = []
-for route in routes:
-    route_list.append(str(route)) 
+def run_car_updater():
+    routes = range(501,513)
+    route_list = []
+    for route in routes:
+        route_list.append(str(route)) 
 
-cars_updated = update_streetcars(route_list)
-print "Update is Complete, %d cars in service" % len(cars_updated)
-remove_out_of_service(cars_updated)
-print "Removal Complete"
+    cars_updated = update_streetcars(route_list)
+    print "Update is Complete, %d cars in service" % len(cars_updated)
+    remove_out_of_service(cars_updated)
+    print "Removal Complete"
+
+def run_stop_updater():
+    print 'Downloading stops'
+    path_to_gvfs = retrieve_data()
+    print 'Download complete'
+    update_stops(path_to_gvfs)
+    rmtree(path_to_gvfs)
+
+parser = argparse.ArgumentParser(description='Update external datasets')
+parser.add_argument('--cars',
+                    action='store_true',
+                    help='Update streetcar location data')
+parser.add_argument('--stops',
+                    action='store_true',
+                    help= 'Update stop location data')
+
+opts = parser.parse_args()
+if opts.cars:
+    run_car_updater()
+if opts.stops:
+    run_stop_updater()
+
+
