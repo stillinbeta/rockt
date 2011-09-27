15e5e24d3baa25388fbd2acf3475f680311e1987
stillinbeta <web@stillinbeta.com>
2011-09-27 14:18:29 -0400

    Added working car detail page

--- urls.py
+++ urls.py
@@ -2,9 +2,12 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 #Django View Helpers
-from django.views.generic import list_detail
+from django.views.generic import DetailView
 from django.views.generic.simple import direct_to_template
 admin.autodiscover()
+
+from rockt.cars.models import Car
+from rockt.stops.models import Stop
 
 
 urlpatterns = patterns('',
@@ -12,6 +15,11 @@ urlpatterns = patterns('',
         (r'^admin/', include(admin.site.urls)),
 	(r'^$', direct_to_template, {'template' : 'map.html'}),
     (r'^locations/$','cars.views.locations'),
+    (r'^car/(?P<slug>.+)/$',
+        DetailView.as_view(
+            model = Car,
+            slug_field = 'number',
+            template_name = 'car.html',)),
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': '/home/sib/Devel/sibcom/static'}),
 
 )
