f9df3344e8e3cbf2277bccd188f87ceeb2ef0db5
stillinbeta <web@stillinbeta.com>
2011-10-11 16:30:18 -0400

    Cleaned up map file

--- cars/views.py
+++ cars/views.py
@@ -11,7 +11,7 @@ def locations(request):
     car_info = dict()
     for car in cars:
         car_info[car.number] = {'lon' : car.location[0],
-                                't' : car.location[1]} 
+                                'lat' : car.location[1]} 
                                              
     return HttpResponse(json.dumps(car_info),mimetype='application/json')
 
