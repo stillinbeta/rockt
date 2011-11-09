ab19aaf9c6972dbaf87c93bf517b02c6a725b64c
stillinbeta <web@stillinbeta.com>
2011-11-09 01:33:38 -0500

    Added map of appropriate height to app

--- app/rockt.js
+++ app/rockt.js
@@ -1,14 +1,36 @@
-var api_url = 'https://rockt.ca/api/'
+//var api_url = 'https://rockt.ca/api/'
+var apiUrl = 'http://localhost:8000/api/'
 
-function add_footer() {
+
+function addFooter() {
     $('.footer').html('stillinbeta | $00');
 }
 
 function init() {
-    add_footer();
+    addFooter();
+    initMap();
+
 }
 
-function error(message) {
+function initMap() {
+    $('#map-content').css('padding','0');
+    $('#map-content').height($('#map').height() - $('#map div:first').height() - 2);
+    var latlng = new google.maps.LatLng(43.6547, -79.3739);
+    var myOptions = { 
+        zoom: 12,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map-content'),
+        myOptions); 
+    var marker = new google.maps.Marker({
+         position: latlng,
+         map: map
+    });
+
+}
+
+function failure(message) {
     $('#error-content').html(message);
     $('<a href="#error" data-rel="dialog" />').click();
 }
@@ -16,17 +38,14 @@ function error(message) {
 
 function loadStops() {
     $.mobile.showPageLoadingMsg();
-    jQuery.ajax( api_url + 'stop/43.64903/-79.3967/')
-    .success(function() { loadedStops(); })
-    .error(function() { error('Oops'); } );
+    jQuery.getJSON( apiUrl + 'stop/find/43.64903/-79.3967/')
+    .success(function(data) { loadedStops(data) } )
+    .error(function(err) { failure('Error retriving stops'); } );
 }
     
-function loadedStops() {
+function loadedStops(data) {
     $.mobile.hidePageLoadingMsg();
-    $.mobile.changePage($('#loaded'));
-}
-
-function go_to_loading() {
+    $.mobile.changePage($('#map'));
 }
 
 $( document ).ready(init);
